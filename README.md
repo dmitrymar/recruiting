996c6a3ab11c77f862d1d5b3f846e3a4064936c9
Dmitry Mardukhayev <dmitrymar@yahoo.com>
2016-12-12 10:53:19 -0500

    initial setup

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # recruiting
+SortOpportunities
+=================
 Recruiting Library
+
+Library Usage
+Create an input 
+```javascript
+var input = `Lead Chef, Chipotle, Denver, CO, 10, 15
+Stunt Double, Equity, Los Angeles, CA, 15, 25
+Manager of Fun, IBM, Albany, NY, 30, 40
+Associate Tattoo Artist, Tit 4 Tat, Brooklyn, NY, 250, 275
+Assistant to the Regional Manager, IBM, Scranton, PA, 10, 15
+Lead Guitarist, Philharmonic, Woodstock, NY, 100, 200`
+```
+
+Call Library Method
+```
+SortOpps.output(input)
+```
---
+++ sortOppsNode.js
@@ -0,0 +1,35 @@
+  var SortOpps = (function () {
+    'use strict';
+
+    function _appendTerm(descArr, self) {
+      return descArr.map(function(el) {
+        return el.map(function(item, index) {
+          var leadingSpace = index === 0 ? "" : " ";
+          return leadingSpace + self.terms[index] + ": " + item;
+        });
+      });
+    }
+
+    function _mapDesc(item) {
+      var splitString = item.split(", ");
+      return [splitString[0], splitString[1], (splitString[2] + ", " + splitString[3]), (splitString[4] + "-" + splitString[5])];
+    }
+
+    function output(input, obj) {
+      var self = obj;
+      var inputArray = input.split("\n").sort();
+      var descArr = inputArray.map(_mapDesc);
+      var termDescArr = _appendTerm(descArr, self);
+
+      return self.header + termDescArr.toString().replace(/,Title:/g, "\nTitle:");
+    }
+
+    return {
+      header: "All Opportunities\n",
+      terms: ["Title", "Organization", "Location", "Pay"],
+      output: function(input) {
+        return output(input, this);
+      }
+    };
+
+  })();
